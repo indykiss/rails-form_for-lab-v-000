ec6e534ae040df3cb37c1d265890210f46cf79f2
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-20 16:52:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-
+  resources :school_classes, only: []
+  resourecs :students, only: []
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
