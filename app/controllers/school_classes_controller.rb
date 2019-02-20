1c07c1c64a8d7f6d595297b3c06b79a197fa14c9
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-20 18:58:48 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -23,9 +23,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
-     @school_class = SchoolClass.find(params[:id])
-     @school_class.update(school_class_params(:title, :room_number))
-     redirect_to school_class_path(@school_class)
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
   end
 
   private
