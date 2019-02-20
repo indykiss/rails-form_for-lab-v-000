ed33d430e766abe2c6eb15e8bedf5fb155d02deb
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-20 18:58:27 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -17,11 +17,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-      @school_class = SchoolClass.new
-  	  @school_class.title = params[:title]
-  	  @school_class.room_number = params[:room_number]
-  	  @school_class.save
-  	  redirect_to school_class_path(@school_class)
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+  	redirect_to school_class_path(@school_class)
   end
 
   def update
