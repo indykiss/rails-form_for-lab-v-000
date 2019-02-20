0a1957f6a5883071f2278a834d3a6b359a979725
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-20 18:35:05 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -1,7 +1,7 @@
 class CreateSchoolClasses < ActiveRecord::Migration
   def change
     create_table :school_classes do |t|
-      t.string :room_number
+      t.integer :room_number
       t.string :title
 
       t.timestamps null: false
