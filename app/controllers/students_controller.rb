0296c398312af7d9eb88ce22e46a53ba037db662
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-20 18:56:53 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -34,6 +34,10 @@ class StudentsController < ApplicationController
     redirect_to student_path(@student)
   end
 
+  private
+  def student_params(*args)
+      params.require(:student).permit(*args)
+  end
 
 
 end
